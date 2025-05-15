7d8189a5f1e1ecb873199dedd3f2fb9ddbec9a3c
rehyan_yadav <189649385+MegaHertzRobotics@users.noreply.github.com>
2025-05-15 18:12:58 +0530

    patterns

--- PATTERN/q1.cpp
+++ PATTERN/q1.cpp
@@ -1,44 +1,23 @@
-// #include <iostream>
-// using namespace std;
-
-// int main() {
-//     // First part: 3 rows of 3 asterisks
-//     for (int i = 0; i < 3; i++) {
-//         cout << "***" << endl;
-//     }
-
-//     // Second part: 5 rows of 5 asterisks
-//     for (int i = 0; i < 5; i++) {
-//         cout << "*****" << endl;
-//     }
-
-//     return 0;
-// }
+//* sqaure pattern
+// no of lines wil be 4 
+/*
+1234
+1234
+1234
+1234
 
 
-
-#include <iostream>
+*/
+#include<iostream>
 using namespace std;
-
-void print1(int n) {
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-           cout<<"*";
-
-        }
-     cout << endl;
-   }
-}
 int main(){
-  int t;
-   cout<<"enter the number of test cases:";
-    cin>>t;
+int n =6;
+for (int i = 0; i<n; i++){
 
-    for(int i=0;i<t;i++){
-    int n;
-    cout<<"enter the number of rows:";
-    cin >>n;
-    print1(n);
+    for (int j = 0; j <n; j++){
+cout<<j+1;
     }
-    return 0;
+    cout<<endl;
+}
+
 }
---
+++ PATTERN/q4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+     int n = 4;
+           int num =1; 
+  for(int i = 0 ; i<n; i++){
+  
+    for (int i =0; i<n; i++){
+cout<<num;
+ num++;
+    }
+cout<<endl;
+
+
+  } 
+  return 0;
+
+}
